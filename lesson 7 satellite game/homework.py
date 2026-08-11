ab5d98821dcdfcbff67a409a0dda993b01a2802c
uddutta108-beep <uddutta108@gmail.com>
2026-08-11 04:10:26 +0100

    Add files via upload

--- lesson 7 satellite game/homework.py
+++ lesson 7 satellite game/homework.py
@@ -5,9 +5,9 @@ import time
 WIDTH = 800
 HEIGHT = 600
 
-satellites = []
-number_of_satellite = 8
-next_satellite = 0
+stars = []
+number_of_star = 8
+next_star = 0
 
 lines = []
 
@@ -15,16 +15,16 @@ start_time = 0
 total_time = 0
 end_time = 0
 
-def create_satellite():
+def create_star():
     global start_time
-    for i in range (number_of_satellite):
-        satellite = Actor("satellite")
-        satellite.pos = random.randint(50,750),random.randint(50,550)
-        satellites.append (satellite)
+    for i in range (number_of_star):
+        star = Actor("star")
+        star.pos = random.randint(50,750),random.randint(50,550)
+        stars.append (star)
 
     start_time = time.time()
 
-create_satellite()
+create_star()
 
 def draw():
     global total_time
@@ -32,23 +32,23 @@ def draw():
 
     number = 1
 
-    for satellite in satellites:
-        satellite.draw()
-        screen.draw.text(str(number),(satellite.pos[0], satellite.pos[1]+20))
+    for star in stars:
+        star.draw()
+        screen.draw.text(str(number),(star.pos[0], star.pos[1]+20))
         number = number + 1 
 
     for line in lines:
         screen.draw.line(line[0], line[1], (255,255,255))
 
-    if next_satellite < number_of_satellite:
+    if next_star < number_of_star:
         total_time = time.time() - start_time
         screen.draw.text(str(round(total_time,1)), (10,10), fontsize = 30)
 
     else:
         screen.draw.text(str(round(total_time,1)), (10,10), fontsize = 30 ) 
 
-    if next_satellite == number_of_satellite:
-        screen.draw.text("Well done", (300,300), fontsize = 50)  
+    if next_star == number_of_star:
+        screen.draw.text("Good job", (300,300), fontsize = 50)  
 
 def update():
     pass
